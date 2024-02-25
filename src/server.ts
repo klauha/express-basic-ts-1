b7d84e751e2e1784c2eb4427b015499b3204267f
Claudia.Alvaro <claudia.alvaro17@gmail.com>
2024-02-25 12:27:05 +0100

    feat:install dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config()
 
 const app = express()
-
-app.listen(4001, () => {
-    console.log('server is running');
+const PORT= process.env.PORT || 4000
+app.listen(PORT, () => {
+    console.log(`server ${PORT} is running`);
 
 
 })
