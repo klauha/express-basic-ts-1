9b50f85983bd7133f3d30977c7e3a6f0ca75678d
Claudia.Alvaro <claudia.alvaro17@gmail.com>
2024-02-25 13:32:16 +0100

    delete Roles created

--- src/controllers/roleControllers.ts
+++ src/controllers/roleControllers.ts
@@ -18,3 +18,21 @@ export const getRole = (req: Request, res: Response) => {
     )
 }
 
+export const updateRole = (req:Request, res:Response) => {
+    res.status(200).json(
+        {
+            success:true,
+            message: 'Role updated'
+        }
+    )
+}
+
+export const deleteRole= (req:Request, res: Response)=> {
+    res.status(200).json (
+        {
+            success: true,
+            message: 'Role deleted'
+        }
+    )
+}
+
