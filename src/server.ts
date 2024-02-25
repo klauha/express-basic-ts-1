359338727852ea0aba55d37e4838005364de24fb
Claudia.Alvaro <claudia.alvaro17@gmail.com>
2024-02-25 12:42:06 +0100

    feat: create healthy enpoint

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,26 @@ dotenv.config()
 
 const app = express()
 const PORT= process.env.PORT || 4000
+
+// primera ruta
+
+app.get('/healthy', (req,res)=> {
+    res.status(200).json(
+        {
+            success:true,
+            messagge:'Serves is healthy',
+    
+        }
+    )
+})
+
+
+
+
+
+
+
+
 app.listen(PORT, () => {
     console.log(`server ${PORT} is running`);
 
